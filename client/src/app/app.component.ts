3c6427f0b18905b6ac05f2c1f6eea5bb05f724bc
nilzbu <nilzbu@gmail.com>
2024-10-16 20:55:28 +0200

    add default routing

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {MatSlideToggle} from '@angular/material/slide-toggle';
+import {NavigationComponent} from './navigation/navigation.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSlideToggle],
+  imports: [RouterOutlet, MatSlideToggle, NavigationComponent, MatInputModule, MatFormFieldModule, MatButtonModule, MatIconModule, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {LoggedInComponent} from './logged-in/logged-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/loggedIn', pathMatch: 'full' },
+  { path: 'loggedIn', component: LoggedInComponent },
+];
